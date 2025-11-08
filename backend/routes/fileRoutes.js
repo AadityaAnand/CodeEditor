2e192c7f947010a6e2d8cb39eec1260abadd0672
Aaditya Anand <aadityaanand271@gmail.com>
2025-11-08 09:12:47 -0500

    chore: update file routes

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const router = express.Router();
 const fileController = require('../controllers/fileController');
-
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+};
+router.use(cors(corsOptions));
 router.post('/projects/:projectId/files', fileController.createFile);
 router.get('/projects/:projectId/tree', fileController.getProjectTree);
 router.get('/projects/:projectId/folders/:folderId', fileController.getFolderContents);
