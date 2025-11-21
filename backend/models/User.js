26daeb82664f39ff3389d1f94bdc8414a10f5cbe
Aaditya Anand <aadityaanand271@gmail.com>
2025-11-21 12:22:27 -0500

    fix: remove duplicate code in User model (prevent crash)

--- backend/models/User.js
+++ backend/models/User.js
@@ -27,29 +27,3 @@ UserSchema.methods.comparePassword = async function (candidatePassword) {
 };
 
 module.exports = mongoose.model('User', UserSchema);
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
